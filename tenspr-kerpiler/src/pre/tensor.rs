ab5567f9656b4175cadf6ddfcc57113308d7c900
Y-jiji <y.jijiji.data.science@gmail.com>
2023-08-11 12:19:00 +0800

    Refactor: seperate unary ops and binary ops

--- tenspr-kerpiler/src/pre/tensor.rs
+++ tenspr-kerpiler/src/pre/tensor.rs
@@ -1,24 +1,44 @@
 use super::*;
-use std::ops::*;
 use smallvec::*;
 
 #[derive(Debug, Clone, Copy)]
+#[non_exhaustive]
+// binary operation that satisfies assication law
+pub enum _Op_ {
+    Add,
+    Mul,
+    Min,
+    Max,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[non_exhaustive]
+// unary operation
+pub enum Op_ {
+    FNeg,
+    FInv,
+    INeg,
+    BNeg,
+}
+
+#[derive(Debug, Clone)]
+#[non_exhaustive]
 pub enum Op {
     Input,
-    Add{lhs: usize, rhs: usize},
-    Mul{lhs: usize, rhs: usize},
-    Sum{oprand: usize, dim: usize},
-    Prd{oprand: usize, dim: usize},
+    Bin { opr: _Op_, lhs: usize, rhs: usize },
+    Red { opr: _Op_, opd: usize, dim: usize },
+    Map { opr: Op_, opd: usize },
+    Mov { opd: usize, map: SmallVec<[usize; 2]> },
 }
 
 impl Op {
-    pub fn upstream(&self) -> SmallVec<[usize; 2]> {
-        match *self {
+    pub fn upstream(&self) -> SmallVec<[usize; 1]> {
+        match self {
             Op::Input => smallvec![],
-            Op::Add{lhs, rhs} => smallvec![lhs, rhs],
-            Op::Mul{lhs, rhs} => smallvec![lhs, rhs],
-            Op::Sum{oprand, ..} => smallvec![oprand],
-            Op::Prd{oprand, ..} => smallvec![oprand],
+            Op::Bin { lhs, rhs, .. } => smallvec![*lhs, *rhs],
+            Op::Map { opd, .. } |
+            Op::Red { opd, .. } |
+            Op::Mov { opd, .. } => smallvec![*opd],
         }
     }
 }
@@ -30,52 +50,3 @@ pub struct TensorRef<'a> {
     pub(super) dtype: DType,
     pub(super) id: usize,
 }
-
-// implment reduce operator for tensor reference
-macro_rules! impl_red_op {
-    ($Name: ident, $name: ident) => {
-        impl<'a> TensorRef<'a> {
-            pub fn $name(&self, dim: usize) -> TensorRef<'a> {
-                let mut pre = self.graph.borrow_mut();
-                let shape = self.shape.clone().remove(dim);
-                let id = pre.push(Op::$Name { oprand: self.id, dim }, self.dtype, shape.clone());
-                drop(pre);
-                return TensorRef { id, graph: self.graph, shape, dtype: self.dtype }
-            }
-        }
-    };
-}
-
-impl_red_op!(Sum, sum);
-impl_red_op!(Prd, prd);
-
-// implment binary operator for tensor reference
-macro_rules! impl_bin_op {
-    ($Name: ident, $name: ident) => {
-        impl<'a> $Name for TensorRef<'a> {
-            type Output = TensorRef<'a>;
-            fn $name(self, rhs: Self) -> Self::Output {
-                let mut pre = self.graph.borrow_mut();
-                assert!(self.dtype == rhs.dtype, "todo");
-                assert!(self.shape == rhs.shape, "todo");
-                let id = pre.push(Op::$Name { lhs: self.id, rhs: rhs.id }, self.dtype, self.shape.clone());
-                drop(pre);
-                return TensorRef { id, graph: self.graph, shape: self.shape.clone(), dtype: self.dtype }
-            }
-        }
-        impl<'a> $Name for &TensorRef<'a> {
-            type Output = TensorRef<'a>;
-            fn $name(self, rhs: Self) -> Self::Output {
-                let mut pre = self.graph.borrow_mut();
-                assert!(self.dtype == rhs.dtype, "todo");
-                assert!(self.shape == rhs.shape, "todo");
-                let id = pre.push(Op::$Name { lhs: self.id, rhs: rhs.id }, self.dtype, self.shape.clone());
-                drop(pre);
-                return TensorRef { id, graph: self.graph, shape: self.shape.clone(), dtype: self.dtype }
-            }
-        }
-    };
-}
-
-impl_bin_op!(Add, add);
-impl_bin_op!(Mul, mul);
--- tenspr-kerpiler/src/pre/typing.rs
+++ tenspr-kerpiler/src/pre/typing.rs
@@ -1,3 +1,5 @@
+use smallvec::*;
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum DType {
     F32, 
@@ -8,15 +10,17 @@ pub enum DType {
 pub enum Dim {Var(usize), Fix(usize)}
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Shape(Vec<Dim>);
+pub struct Shape(SmallVec<[Dim; 2]>);
 
 impl Shape {
     pub fn remove(mut self, dim: usize) -> Self {
-        self.0.remove(dim);
-        return self;
+        self.0.remove(dim); self
+    }
+    pub fn insert(mut self, dim: usize, n: usize) -> Self {
+        self.0.insert(dim, Dim::Fix(n)); self
     }
 }
 
 pub fn sh<const N: usize>(x: [usize; N]) -> Shape {
-    Shape(x.map(|i| Dim::Fix(i)).into())
+    Shape(x.map(|i| Dim::Fix(i)).to_smallvec())
 }
